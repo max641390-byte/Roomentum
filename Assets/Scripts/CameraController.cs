cfe02d5b05d200b3a4381abc13db06d61523551a
Vincent314159265 <vxib@proton.me>
2026-09-18 13:43:36 +0200

    Added rooms and different camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    private Vector3 _target;
     [SerializeField] private Vector3 _offset;
     [SerializeField] private float _smoothing;
+    [SerializeField] private Transform currentRoom;
+
+    void Start()
+    {
+        UpdateCurrentRoom(currentRoom);
+    }
 
     void FixedUpdate()
     {
-        Vector3 newPosition = Vector3.Lerp(transform.position, _target.position + _offset, _smoothing * Time.deltaTime);
+        Vector3 newPosition = Vector3.Lerp(transform.position, _target + _offset, _smoothing * Time.deltaTime);
         transform.position = newPosition;
+    }
+
+    public Transform GetCurrentRoom()
+    {
+        return currentRoom;
+    }
+
+    public void UpdateCurrentRoom(Transform newRoom)
+    {
+        currentRoom = newRoom;
+        _target = currentRoom.position;
     }
 }
