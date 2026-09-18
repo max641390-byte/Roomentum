2ec0bca007cb338e8e48287f851223f6bf7e4da0
Vincent314159265 <vxib@proton.me>
2026-09-18 15:58:15 +0200

    Revert "Added rooms and different camera"
    This reverts commit cfe02d5b05d200b3a4381abc13db06d61523551a.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,30 +2,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Vector3 _target;
+    [SerializeField] private Transform _target;
     [SerializeField] private Vector3 _offset;
     [SerializeField] private float _smoothing;
-    [SerializeField] private Transform currentRoom;
-
-    void Start()
-    {
-        UpdateCurrentRoom(currentRoom);
-    }
 
     void FixedUpdate()
     {
-        Vector3 newPosition = Vector3.Lerp(transform.position, _target + _offset, _smoothing * Time.deltaTime);
+        Vector3 newPosition = Vector3.Lerp(transform.position, _target.position + _offset, _smoothing * Time.deltaTime);
         transform.position = newPosition;
-    }
-
-    public Transform GetCurrentRoom()
-    {
-        return currentRoom;
-    }
-
-    public void UpdateCurrentRoom(Transform newRoom)
-    {
-        currentRoom = newRoom;
-        _target = currentRoom.position;
     }
 }
